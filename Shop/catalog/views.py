e67a6509fe640759cde7857c86c5afd85998acaa
ignat <grischuk93@gmail.com>
2024-04-03 21:21:28 +0300

    add user cart

--- Shop/catalog/views.py
+++ Shop/catalog/views.py
@@ -1,10 +1,13 @@
-from .models import Category, Product, Seller, Discount
+from .models import Category, Product, Seller, Discount, Cart
 from rest_framework.generics import ListAPIView
 from .serializers import (CategorySerializer, ProductSerializer,
-                          SellerSerializer, DiscountSerializer)
-from rest_framework.permissions import AllowAny
+                          SellerSerializer, DiscountSerializer,
+                          AddProductSerializer, CartSerializer,
+                          DeleteProductSerializer)
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.db.models import F
 
 
 class CategoryListView(ListAPIView):
@@ -23,9 +26,9 @@ class CategoryProductsView(APIView):
 
 
 class SellerListView(APIView):
-        queryset = Seller.objects.all()
-        serializer_class = SellerSerializer
-        permission_classes = (AllowAny, )
+    queryset = Seller.objects.all()
+    serializer_class = SellerSerializer
+    permission_classes = (AllowAny, )
 
 
 class SellerProductView(APIView):
@@ -35,3 +38,62 @@ class SellerProductView(APIView):
         queryset = Product.objects.filter(seller_id=seller_id)
         serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data)
+
+
+class DiscountListView(ListAPIView):
+    queryset = Discount.objects.all()
+    serializer_class = DiscountSerializer
+    permission_classes = (AllowAny,)
+
+
+class DiscountProductsView(APIView):
+    permission_classes = (AllowAny, )
+
+    def get(self, request, discount_id):
+        queryset = Product.objects.filter(discount__id=discount_id)
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class CartView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def post(self, request):
+        input_serializer = AddProductSerializer(data=request.data)
+        input_serializer.is_valid(raise_exception=True)
+        product = Product.objects.get(id=input_serializer.data["product_id"])
+        cart_object, _ = Cart.objects.get_or_create(user=request.user,
+                                                    product=product)
+        if cart_object.count:
+            cart_object.count += input_serializer.data["count"]
+        else:
+            cart_object.count = input_serializer.data["count"]
+
+        if cart_object.count <= 0:
+            cart_object.delete()
+        else:
+            cart_object.save()
+
+        return Response()
+
+    def get(self, request):
+        user = request.user
+        cart = Product.objects.prefetch_related(
+            "cart_set"
+        ).filter(cart__user=user).values(
+            "name", "price", "discount",
+            discount_percent=F("discount__percent"),
+            count=F("cart__count"), discount_date_end=F("discount__date_end")
+        )
+        serializer = CartSerializer({"products": cart})
+        return Response(serializer.data)
+
+
+    def delete(self, request):
+        input_serializer = DeleteProductSerializer(data=request.data)
+        input_serializer.is_valid(raise_exception=True)
+
+        product = Product.objects.get(id=input_serializer.data["product_id"])
+        Cart.objects.get(user=request.user, product=product).delete()
+
+        return Response()
